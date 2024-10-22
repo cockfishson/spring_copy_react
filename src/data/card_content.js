183571e4eb6b8b3273cc7c5061636a8d445ac256
Soskfish <fox25055@gmail.com>
2024-10-22 20:14:24 +0300

    feat: Added data required for menu's and cards

---
+++ src/data/card_content.js
@@ -0,0 +1,38 @@
+export const CARDS_DEFAULT = [
+  {
+    imgSrc: "./media/spring-boot.svg",
+    title: "Spring Boot",
+    description:
+      "Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible.",
+  },
+  {
+    imgSrc: "./media/spring-framework.svg",
+    title: "Spring Framework",
+    description:
+      "Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more.",
+  },
+  {
+    imgSrc: "./media/spring-data.svg",
+    title: "Spring Data",
+    description:
+      "Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond.",
+  },
+  {
+    imgSrc: "./media/spring-cloud.svg",
+    title: "Spring Cloud",
+    description:
+      "Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices.",
+  },
+  {
+    imgSrc: "./media/spring-data-flow.svg",
+    title: "Spring Cloud Data Flow",
+    description:
+      "Provides an orchestration service for composable data microservice applications on modern runtimes.",
+  },
+  {
+    imgSrc: "./media/spring-security.svg",
+    title: "Spring Security",
+    description:
+      "Protects your application with comprehensive and extensible authentication and authorization support.",
+  },
+];
